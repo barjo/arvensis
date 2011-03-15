0ef377c34537323d69ce1b527d3f3f5451ec4113
barjo <astrosus@gmail.com>
2011-03-15 22:54:47 +0100

    Add core project.

---
+++ core/src/main/java/org/ow2/chameleon/rose/EndpointFactory.java
@@ -0,0 +1,10 @@
+package org.ow2.chameleon.rose.core;
+
+/**
+ * The EndpointFactory service are provided by the endpoint-creator component.
+ * It allows for supervisor component to create the endpoint thanks to the ServiceReference.   
+ **/
+public interface EndpointFactory {
+
+}
+ 

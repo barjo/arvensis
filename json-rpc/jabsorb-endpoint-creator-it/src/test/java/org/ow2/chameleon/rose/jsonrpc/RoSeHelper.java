14ee8241ad5e6c41ec3979b429f749862909a983
barjo <astrosus@gmail.com>
2011-04-11 11:24:33 +0200

    Rename ITTools in RoSeHelper.

--- json-rpc/jabsorb-endpoint-creator-it/src/test/java/org/ow2/chameleon/rose/jsonrpc/RoSeHelper.java
+++ json-rpc/jabsorb-endpoint-creator-it/src/test/java/org/ow2/chameleon/rose/jsonrpc/RoSeHelper.java
@@ -20,12 +20,19 @@ import org.apache.felix.ipojo.ConfigurationException;
 import org.apache.felix.ipojo.Factory;
 import org.apache.felix.ipojo.MissingHandlerException;
 import org.apache.felix.ipojo.UnacceptableConfiguration;
+import org.osgi.framework.BundleContext;
 import org.osgi.framework.ServiceReference;
+import org.osgi.framework.ServiceRegistration;
 import org.ow2.chameleon.testing.helpers.OSGiHelper;
 
-public class ITTools {
+public class RoSeHelper {
+	private final BundleContext context;
 
-    /**
+    public RoSeHelper(BundleContext pContext) {
+		context = pContext;
+	}
+
+	/**
      * Create an instance with the given factory and properties
      * @param factory
      * @param properties
@@ -97,6 +104,42 @@ public class ITTools {
         }
     }
 
+    public <T> ServiceRegistration registerService(T service,Class<T> klass){
+    	return context.registerService(klass.getName(), service, null);
+    }
+    
+    @SuppressWarnings("unchecked")
+	public <T> T getServiceObject(Class<T> klass, String filter) {
+		ServiceReference[] srefs = null;
+		
+		try {
+			srefs = context.getAllServiceReferences(klass.getName(), filter);
+		} catch (Exception e) {
+			e.printStackTrace();
+			return null;
+		}
+		
+		if (srefs != null && srefs.length > 0) {
+			return (T) context.getService(srefs[0]);
+		} 
+		else {
+			return null;
+		}
+	}
+
+	@SuppressWarnings("unchecked")
+	public <T> T getServiceObject(Class<T> klass) {
+		ServiceReference sref = null;
+
+		sref = context.getServiceReference(klass.getName());
+
+		if (sref != null) {
+			return (T) context.getService(sref);
+		} else {
+			return null;
+		}
+	}
+    
 /*    public static Map doGet(final String gwurl,final JSONService jsonservice) throws Exception {
         Map map;
         // Send data

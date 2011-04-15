1e9652502a2ab0c248906907fc63463ef256b48e
barjo <astrosus@gmail.com>
2011-04-15 18:50:02 +0200

    Fix name.

---
+++ core/src/main/java/org/ow2/chameleon/rose/registry/ExportRegistryProvisioning.java
@@ -0,0 +1,18 @@
+package org.ow2.chameleon.rose.registry;
+
+import org.osgi.service.remoteserviceadmin.ExportReference;
+
+
+/**
+ * This service allows to register a newly available endpoint within an RRegistry.
+ * @author barjo
+ */
+public interface ExportRegistryProvisioning {
+	
+	void put(Object key, ExportReference xref);
+	
+	ExportReference remove(Object key);
+	
+	boolean contains(Object key);
+}
+

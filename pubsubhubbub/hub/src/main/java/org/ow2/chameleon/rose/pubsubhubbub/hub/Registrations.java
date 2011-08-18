1e4b0b0709d7e65b473dd9c3e2a4bf7a864de259
Bartek <Bartek@Bartek-PC>
2011-08-18 12:01:50 +0200

    add a simple implementation of synchronization system to hub

--- pubsubhubbub/hub/src/main/java/org/ow2/chameleon/rose/pubsubhubbub/hub/Registrations.java
+++ pubsubhubbub/hub/src/main/java/org/ow2/chameleon/rose/pubsubhubbub/hub/Registrations.java
@@ -4,6 +4,7 @@ import java.util.HashSet;
 import java.util.Map;
 import java.util.Set;
 import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.locks.Lock;
 
 import org.osgi.service.remoteserviceadmin.EndpointDescription;
 
@@ -13,6 +14,7 @@ public class Registrations {
 	private Map<String, Set<EndpointDescription>> topics;
 	// subscriber name(callBackUrl), filter
 	private Map<String, EndpointsByFilter> subscribers;
+	private Lock lock;
 
 	public Registrations() {
 		topics = new ConcurrentHashMap<String, Set<EndpointDescription>>();
@@ -47,7 +49,7 @@ public class Registrations {
 	 */
 	public Set<EndpointDescription> getEndpointsForCallBackUrl(
 			String callBackUrl) {
-
+		lock.lock();
 		Set<EndpointDescription> matchedEndpointDescriptions = new HashSet<EndpointDescription>();
 		String filter = subscribers.get(callBackUrl).getFilter();
 
@@ -57,12 +59,13 @@ public class Registrations {
 				subscribers.get(callBackUrl).addEndpoint(endpoint);
 			}
 		}
+		lock.unlock();
 		return matchedEndpointDescriptions;
 	}
 
 	public Set<String> getSubscribersByEndpoint(EndpointDescription endp) {
 		Set<String> matchedSubscribers = new HashSet<String>();
-
+		lock.lock();
 		for (String subscriber : subscribers.keySet()) {
 
 			if (endp.matches(subscribers.get(subscriber).getFilter())) {
@@ -70,6 +73,7 @@ public class Registrations {
 				subscribers.get(subscriber).addEndpoint(endp);
 			}
 		}
+		lock.unlock();
 		return matchedSubscribers;
 	}
 
@@ -77,6 +81,7 @@ public class Registrations {
 			String callBackUrl) {
 
 		Map<String, Set<EndpointDescription>> subscriberEndpoints = new ConcurrentHashMap<String, Set<EndpointDescription>>();
+		lock.lock();
 		for (String subscriber : subscribers.keySet()) {
 			subscriberEndpoints.put(subscriber,
 					new HashSet<EndpointDescription>());
@@ -87,21 +92,24 @@ public class Registrations {
 				}
 			}
 		}
+		lock.unlock();
 		return subscriberEndpoints;
 	}
 
 	public Set<EndpointDescription> getAllEndpoints() {
 
 		Set<EndpointDescription> allEndpoints = new HashSet<EndpointDescription>();
+		lock.lock();
 		for (String publisher : topics.keySet()) {
 			allEndpoints.addAll(topics.get(publisher));
 		}
-
+		lock.unlock();
 		return allEndpoints;
 	}
 	
 	
 	public void clearTopic(String rssURL) {
+		lock.lock();
 		for (EndpointDescription endpoint : topics.get(rssURL)) {
 
 			for (String subscriber : subscribers.keySet()) {
@@ -109,12 +117,14 @@ public class Registrations {
 			}
 		}
 		topics.remove(rssURL);
-
+		lock.unlock();
 	}
 
 	public void removeInterestedEndpoint(String callBackUrl,
 			EndpointDescription edp) {
+		lock.lock();
 		subscribers.get(callBackUrl).removeEndpoint(edp);
+		lock.unlock();
 	}
 	
 

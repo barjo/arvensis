3ac105d574e6c6adedb156195e13bf92098b444e
barjo <astrosus@gmail.com>
2011-09-30 18:29:52 +0200

    Convenient helloworld example which used jaxrs annotations.

---
+++ example/hello-rest/src/main/java/rose/example/jaxrs/contract/HelloWorld.java
@@ -0,0 +1,12 @@
+package rose.example.jaxrs.contract; 
+
+
+public interface HelloWorld {
+
+	/**
+	 * @return <code>"Hello "+name+ "!"</code> 
+	 **/
+	String hello(String name);
+
+}
+
---
+++ example/hello-rest/src/main/java/rose/example/jaxrs/internal/HelloImpl.java
@@ -0,0 +1,26 @@
+package rose.example.jaxrs.internal;
+
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.Produces;
+
+import org.apache.felix.ipojo.annotations.Component;
+import org.apache.felix.ipojo.annotations.Instantiate;
+import org.apache.felix.ipojo.annotations.Provides;
+
+import rose.example.jaxrs.contract.HelloWorld;
+
+@Component(name="example_helloworld")
+@Instantiate(name="example_helloworld-1")
+@Provides
+@Path(value="/helloworld/{name}")
+public class HelloImpl implements HelloWorld {
+
+	@GET
+	@Produces("text/plain")
+	public String hello(@PathParam("name") String name) {
+		return "Hello "+name+" !";
+	}
+
+}

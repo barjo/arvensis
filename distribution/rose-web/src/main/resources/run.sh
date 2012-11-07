a2561fdfb1a8c53b2314cf4293d2f948357b08cd
barjo <astrosus@gmail.com>
2012-11-07 15:05:05 +0100

    Add rose web distribution.

---
+++ distribution/rose-web/src/main/resources/run.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+
+echo "\n"
+echo "\t==========================================="
+echo "\t=                                         ="
+echo "\t= @}-'-,-- RoSe Web Distribution --,-'-{@ ="
+echo "\t=                                         ="
+echo "\t==========================================="
+echo "\n"
+
+java -jar core/org.apache.felix.main-4.0.3.jar $@
+
